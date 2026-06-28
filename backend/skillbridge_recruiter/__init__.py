252b0722cdf58bd06ed934db6bb33abbc03fe7ec
AnuranjanJain <AnuranjanJain@users.noreply.github.com>
2026-06-28 13:41:40 +0530

    Deploy SkillBridge Recruiter to GitHub Pages

--- backend/skillbridge_recruiter/__init__.py
+++
@@ -1,6 +0,0 @@
-"""SkillBridge Recruiter backend package."""
-
-from .jd import analyze_job_text
-from .ranker import ScoringWeights, rank_candidates
-
-__all__ = ["ScoringWeights", "analyze_job_text", "rank_candidates"]
